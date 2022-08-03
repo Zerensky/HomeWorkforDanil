10970c200821f51a4cbb4a004450e438cc7dd853
Zerensky <pure.evil97@mail.ru>
2022-08-03 19:55:51 +0600

    первая домашняя работа

---
+++ HW1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int summa = 1;
+for (int i = 1; i <= B; i++)
+    {
+        summa = summa * A;
+    }
+Console.WriteLine(summa);
